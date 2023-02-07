29d95e15bbe6de5b40272ceb04faaaae3550408c
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-07 19:14:41 +0500

    tabs component created

---
+++ src/components/burger-ingredients/ui/tabs/tabs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./tabs.module.css"
+import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
+
+function Tabs({names}) {
+    const [current, setCurrent] = React.useState("Булки")
+    return (
+        <div className={styles.tabs}>
+            {names.map(title =>
+                <Tab key={title} value={title} active={current === title} onClick={setCurrent}>
+                    {title}
+                </Tab>)
+            }
+    </div>);
+}
+
+export default Tabs;
