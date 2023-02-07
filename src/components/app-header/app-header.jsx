b20ce329d71b96ad42547b32296f83351aecc830
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-07 19:20:03 +0500

    after navbutton changing

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import styles from './app-header.module.css'
-import {Button, Logo, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Logo, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import {NavButton} from "./ui/nav-button/nav-button";
 
-const  AppHeader = (props) => {
+const AppHeader = () => {
     return (
         <>
            <header className={styles.header}>
-                <div className={styles.headerContainer + " pt-4 pb-4"}>
-                         <NavButton>
-                             <BurgerIcon type="primary" />
-                             <p className="text text_type_main-default ml-2">
-                                 Конструктор
-                             </p>
-                         </NavButton>
-                         <NavButton>
-                             <ListIcon type="secondary" />
-                             <p className="text text_type_main-default text_color_inactive ml-2">
-                                 Лента заказов
-                             </p>
-                         </NavButton>
+                <nav className={styles.headerContainer + " pt-4 pb-4"}>
+                     <NavButton text={"Конструктор"} active={true}>
+                         <BurgerIcon type="primary" />
+                     </NavButton>
+                     <NavButton text={"Лента заказов"} active={false}>
+                         <ListIcon type="secondary" />
+                     </NavButton>
                     <div className={styles.logo}>
                         <Logo/>
                     </div>
-                    <NavButton>
+                    <NavButton text={"Личный кабинет"} active={false}>
                         <ProfileIcon type="secondary" />
-                        <p className="text text_type_main-default text_color_inactive ml-2">
-                            Личный кабинет
-                        </p>
                     </NavButton>
-                </div>
+                </nav>
            </header>
         </>
     );
