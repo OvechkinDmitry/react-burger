89d428f582588e36ed3f0a3be515e355abc63486
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-07 19:15:39 +0500

    changed navbutton layout

--- src/components/app-header/ui/nav-button/nav-button.jsx
+++ src/components/app-header/ui/nav-button/nav-button.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './nav-button.module.css'
 
-export const NavButton = (props) => {
+export const NavButton = ({text, active, children}) => {
+    const isActive = active ? "" : "text_color_inactive"
     return (
-        <div className={styles.navBtn + " pl-5 pt-4 pb-4 pr-5"}>
-            {props.children}
-        </div>
+        <a href={"#"} className={`${styles.navBtn} pl-5 pt-4 pb-4 pr-5`}>
+            {children}
+            <p className={`text text_type_main-default ${isActive} ml-2`}>
+                {text}
+            </p>
+        </a>
     );
 };
