983d9275a8d43fd91465410fea6e40363b02aeda
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-10 23:18:21 +0500

    added comment

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -7,6 +7,7 @@ const IngredientDetails = ({id}) => {
     return (<>
             <p className={`${styles.id} text text_type_digits-large`}>{id}</p>
             <p className="text text_type_main-medium mt-8">идентификатор заказа</p>
+            {/*в библиотеке компонентов есть лого только шапки, поэтому вставил изображение*/}
             <img className={'mt-15'} src={image} alt={"done"}/>
             <p className="text text_type_main-small mt-15">Ваш заказ начали готовить</p>
             <p className="text text_type_main-small text_color_inactive mt-2 pb-30">Дождитесь готовности на орбитальной
