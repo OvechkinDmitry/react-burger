8da7126fc0f503eace26f74573729064d07f560d
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-10 23:53:35 +0500

    ui Done component created

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './ingredient-details.module.css'
-import image from '../../images/modal/done.png'
 import PropTypes from "prop-types";
+import Done from "../ui/done/done";
 
 const IngredientDetails = ({id}) => {
     return (<>
             <p className={`${styles.id} text text_type_digits-large`}>{id}</p>
             <p className="text text_type_main-medium mt-8">идентификатор заказа</p>
-            {/*в библиотеке компонентов есть лого только шапки, поэтому вставил изображение*/}
-            <img className={'mt-15'} src={image} alt={"done"}/>
+            <Done extraClass={"mt-15"}/>
             <p className="text text_type_main-small mt-15">Ваш заказ начали готовить</p>
             <p className="text text_type_main-small text_color_inactive mt-2 pb-30">Дождитесь готовности на орбитальной
                 станции</p>
