e8c91841f7f6eeafebdad8f334ded794d2a2caca
Dmitry Ovechkin <ovechkin_dima03@mail.ru>
2023-02-17 16:21:10 +0500

    refactored requests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,13 @@
-export const URL_INGREDIENTS = "https://norma.nomoreparties.space/api/ingredients"
-export const URL_ORDER = "https://norma.nomoreparties.space/api/orders"
+import axios from "axios";
+
+export const NomorepartiesInstance = axios.create({
+    baseURL: 'https://norma.nomoreparties.space/api/',
+    timeout: 0,
+    headers: {
+        Accept: 'application/json',
+    }
+});
+
+
+export const URL_INGREDIENTS = "/ingredients"
+export const URL_ORDER = "/orders"
--- src/utils/post-order.js
+++ src/utils/post-order.js
@@ -1,15 +1,11 @@
-import axios from "axios";
-import {URL_ORDER} from "./constants";
+import {NomorepartiesInstance, URL_ORDER} from "./constants";
 
 export const postOrder = async (idS) => {
-    try{
-        const res = await axios.post(URL_ORDER, {"ingredients": idS})
-        if(res.status === 200){
-            const {name, order} = res.data
-            return {name, id: order.number}
-        }
+    const res = await NomorepartiesInstance.post(URL_ORDER, { ingredients: idS });
+    if (res.status === 200) {
+        const { name, order } = res.data;
+        return { name, id: order.number };
+    } else {
+        return Promise.reject(`Ошибка ${res.status}`);
     }
-    catch (e){
-        console.log(`Ошибка:${e}`)
-    }
-}
+};
